fb84dc7fc0e0eb0fd7c0d398140c21892cab9902
Marcus de Mendes Caldas Raymundo Reaiche <marcusmendes@bndes.gov.br>
2020-04-12 23:19:22 -0300

    Part 2 - Templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        "author": "Marcus Reaiche",
+        "title": "Blog Post 1",
+        "content": "First post content",
+        "date_posted": "April 12, 2020"
+    },
+    {
+        "author": "John Doe",
+        "title": "Blog Post 2",
+        "content": "Second post content",
+        "date_posted": "April 12, 2020"
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template("about.html", title="About")
 
 
 if __name__ == "__main__":
