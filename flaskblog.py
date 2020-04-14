6ffa98f6a69f37fa3cdfa298eebe5c7bfc7a0bc0
Marcus de Mendes Caldas Raymundo Reaiche <marcusmendes@bndes.gov.br>
2020-04-13 23:33:12 -0300

    Part 3 - Forms and User Input

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "e537a8bc61e1b4aff62b664c42c2889a"
 
 posts = [
     {
@@ -25,6 +29,28 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html", title="About")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}.", "success")
+        return redirect(url_for("home"))
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@email.com" and form.password.data == "password":
+            flash(f"Successfully logged in", "success")
+            return redirect(url_for("home"))
+        else:
+            flash(f"Unable to log in. Please check your email and password.", "danger")
+
+    return render_template("login.html", title="Login", form=form)
 
 
 if __name__ == "__main__":
