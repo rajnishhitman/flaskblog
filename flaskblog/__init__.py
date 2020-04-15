592c5f2a53f7c9650f39796c62fbd78d4721e8a1
Marcus de Mendes Caldas Raymundo Reaiche <marcusmendes@bndes.gov.br>
2020-04-14 23:46:24 -0300

    Part 6 - User Authentication

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
 app = Flask(__name__)
 # Configuration
 app.config["SECRET_KEY"] = "e537a8bc61e1b4aff62b664c42c2889a"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+# Database
 db = SQLAlchemy(app)
+# bcrypt for hashing and verifying passwords
+bcrypt = Bcrypt()
+# Login manager
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
 
 from flaskblog import routes
-
--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,8 +1,13 @@
+import re
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, EqualTo, Email
+from wtforms.validators import DataRequired, Length, EqualTo, Email, ValidationError
+
+from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
+    email_pattern = re.compile(r"(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%¨&\*\(\)])(?=.{6,})")
+
     username = StringField("Username", validators=[
         DataRequired(),
         Length(min=2, max=20)
@@ -19,6 +24,20 @@ class RegistrationForm(FlaskForm):
         EqualTo("password")
     ])
     submit = SubmitField("Sign Up")
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("This username is taken. Please choose another one.")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("This email is taken. Please choose another one.")
+
+    def validate_password(self, password):
+        if re.search(self.email_pattern, password.data) is None:
+            raise ValidationError("Password field must have at least 6 characters including at least one digit, one lowercase letter, one capital letter, and one special character (!, @, #, $, %, ¨, &)")
 
 
 class LoginForm(FlaskForm):
